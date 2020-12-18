551e80fa925a21009e231988b68a320b657fe071
Shahzeb Shahid <srehanshahid95@gmail.com>
2020-12-18 19:20:45 +0530

    Update Vanya_and_Fence.java

--- Div2-A/Vanya_and_Fence.java
+++ Div2-A/Vanya_and_Fence.java
@@ -1,1 +1,34 @@
 
+// Problem URL - CF677-D2-A : https://codeforces.com/contest/677/problem/A
+
+
+import java.util.Scanner;
+ 
+public class Solve {
+ 
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int h = scan.nextInt();
+		int arr[] = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			arr[i] = scan.nextInt();
+		}
+		int result = 0;
+		
+		for(int i=0; i<n; i++) {
+			if(arr[i] <= h) {
+				result++;
+			}else {
+				result = result + 2;
+			}
+		}
+		
+		System.out.println(result);
+		
+	}
+ 
+}
