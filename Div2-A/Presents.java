96de7909960fc347889f345ba6d818883b424843
Shahzeb Shahid <srehanshahid95@gmail.com>
2020-12-28 20:38:13 +0530

    Create Presents.java

---
+++ Div2-A/Presents.java
@@ -0,0 +1,30 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		int arr[] = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			arr[i] = scan.nextInt();
+		}
+		
+		int res[] = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			res[arr[i]-1] = i+1;
+		}
+		
+		for(int i=0; i<n; i++) {
+			System.out.print(res[i] + " ");
+		}
+	}
+
+}
